28d355bbd0a58d9b8ce7fd27d45576fd73d68980
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2015-10-21 08:53:36 +0800

    remove the cashier add customer system todo:register

--- resources/views/User/usrBettingList.blade.php
+++ resources/views/User/usrBettingList.blade.php
@@ -18,6 +18,7 @@
                         <td>号码</td>
                         <td>投注金额</td>
                         <td>中奖金额</td>
+                        <td>状态</td>
                         <td>投注时间</td>
                     </tr>
                     @if (count($lu_lotteries_k3s))
@@ -28,6 +29,13 @@
                                 <td>{{ $lu_lotteries_k3->codes }}</td>
                                 <td>{{ $lu_lotteries_k3->eachPrice }}</td>
                                 <td>{{ $lu_lotteries_k3->bingoPrice }}</td>
+                                <td>
+                                    @if($lu_lotteries_k3->status == 1)
+                                        等待开奖
+                                    @else
+                                        已开奖
+                                    @endif
+                                </td>
                                 <td>{{ $lu_lotteries_k3->created_at }}</td>
                             </tr>
                         @endforeach
