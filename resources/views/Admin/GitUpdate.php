9033ee134984b6057a4fa5213a00cf7973370276
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2015-11-27 15:41:09 +0800

    remove the cashier add customer system todo:register

--- resources/views/Admin/GitUpdate.php
+++ resources/views/Admin/GitUpdate.php
@@ -1,7 +1,9 @@
 <?php
 exec('/usr/bin/cd /var/www/html/LaravelLuna | /usr/bin/git pull origin master');
 
-//$fp = @popen('usr/bin/git pull origin master', "r");
-//echo($fp);
-//@pclose($fp);
+$fp = @popen('/usr/bin/cd /var/www/html/LaravelLuna | /usr/bin/git pull origin master', "r");
+if($fp){
+    echo($fp);
+}
+@pclose($fp);
 echo "second<br>success";
