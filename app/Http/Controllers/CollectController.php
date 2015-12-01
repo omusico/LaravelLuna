b5f46172c8d44ea2fed3ffa30dcd0f54661e3e51
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2015-12-01 15:25:07 +0800

    remove the cashier add customer system todo:register

--- app/Http/Controllers/CollectController.php
+++ app/Http/Controllers/CollectController.php
@@ -30,14 +30,8 @@ class CollectController extends Controller
     //采集
     public function collectLotteryData(Request $request)
     {
+
         $lotteryType = strtoupper(trim($request->lottery_type));
-        $this->collectFromType($lotteryType);
-    }
-
-
-    public function collectFromType($lotteryType)
-    {
-
         $lunaFunctions = new LunaFunctions();
         $configs = defaultCache::cache_lottery_status();
         $conf = $lunaFunctions->get_lottery_config($lotteryType);
@@ -82,9 +76,6 @@ class CollectController extends Controller
         } else {
             $timeData = $lunaFunctions->get_current_period($lotteryType);
         }
-//        $lotteryResult = Waf::model('lottery/result');
-//        $caijiModel = Waf::model('common/caijirecord');
-//        $count = $lotteryResult->isExistsProName($timeData['prePeriod'],$lotteryType);
         $count = lu_lotteries_result::where('proName', $timeData['prePeriod'])->where('typeName', $lotteryType)->count();
         $now = strtotime("now");
         $periodTime = 600;
@@ -232,8 +223,23 @@ class CollectController extends Controller
         if ($hadKj) {
             $this->syncCjFromNotice($lotteryType, $timeData['prePeriod'], $kjData['preOpenResult'], $source);
             // 通知其他站点
+//            $base = Waf_Config::get('base');
+//            $theme = $base['theme'];
+//
+//            $mp=new MultiHttpRequest();
+//            $urls = Waf::cache("notify_url");
+//            $newUrls = array();
+//            foreach ($urls as $key=>$val){
+//                $newUrls[$key] = $val."/index.php?m=common&c=index&a=syncCjFromNotice".
+//                    "&lottery_type=".$lotteryType."&prePeriod=".$timeData['prePeriod']."&preOpenResult=".$kjData['preOpenResult'].'&from='.$source;
+//            }
+//
+//            $mp->set_urls($newUrls);
+//            $contents = $mp->start();
+
         }
         return $result;
+//        $this->response->throwJson($result);
     }
 
     // 根据通知来开奖派奖
