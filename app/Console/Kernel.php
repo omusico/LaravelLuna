66d762ddff3a806ca2970d169821cc2604edf402
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2015-12-02 15:36:05 +0800

    remove the cashier add customer system todo:register

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,9 +29,9 @@ class Kernel extends ConsoleKernel {
 //        $schedule->call('Common\CommonClass@cronCollect')->cron('*/1 * * * * *');
         $schedule->call(function () {
             if (\Cache::has("CountCron")) {
-                Cache::forever('CountCron', date('Y-m-d H:i:s'));
+                \Cache::forever('CountCron', date('Y-m-d H:i:s'));
             }else{
-                Cache::Add('CountCron', date('Y-m-d H:i:s'),2);
+                \Cache::Add('CountCron', date('Y-m-d H:i:s'),2);
             }
             if(env("SITE_TYPE")=="five"){
                 $fiveArrs=['sdfive','gdfive','shfive','zjfive','jxfive','liaoningfive','hljfive'];
