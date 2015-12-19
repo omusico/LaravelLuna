187efa717da04a806f5b7be18faf315b3e13d892
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2015-12-19 11:01:09 +0800

    remove the cashier add customer system todo:register

--- resources/views/favourable.blade.php
+++ resources/views/favourable.blade.php
@@ -20,17 +20,22 @@
 @section('content')
     <div class="container" style="text-align: center;">
         {{--@if(env('SITE_TYPE','')=='five')--}}
-            {{--<div class="row">--}}
-                {{--<div class="col-md-10 col-md-offset-1" style="background-color: white;padding:0px">--}}
-                    {{--@endif--}}
-                    {{--<div class="cm_wrapper clear">--}}
-                        {{--<div class="cm_w1000" style="width: 100%;background-color: transparent"><img src="/css/favourable.gif" alt="优惠活动"--}}
-                                                   {{--width="1000" height="100%"></div>--}}
-                    {{--</div>--}}
-                    <textarea class="form-control" name="favor" rows="30" readonly>{{Cache::get('favor')}}</textarea>
-                    {{--@if(env('SITE_TYPE','')=='five')--}}
-                {{--</div>--}}
-            {{--</div>--}}
+        {{--<div class="row">--}}
+        {{--<div class="col-md-10 col-md-offset-1" style="background-color: white;padding:0px">--}}
+        {{--@endif--}}
+        {{--<div class="cm_wrapper clear">--}}
+        {{--<div class="cm_w1000" style="width: 100%;background-color: transparent"><img src="/css/favourable.gif" alt="优惠活动"--}}
+        {{--width="1000" height="100%"></div>--}}
+        {{--</div>--}}
+        @if(env("SITE_TYPE")=="five")
+            <div style="width: 100%;background-color: transparent"><img src="/css/fivefav.png" alt="优惠活动"
+            width="100%" height="100%"></div>
+        @else
+            <textarea class="form-control" name="favor" rows="30" readonly>{{Cache::get('favor')}}</textarea>
+        @endif
+        {{--@if(env('SITE_TYPE','')=='five')--}}
+        {{--</div>--}}
+        {{--</div>--}}
         {{--@endif--}}
     </div>
 @stop
