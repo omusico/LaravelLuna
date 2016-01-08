b5d9224b40bf356d0582eaafbf44ed7e746a496f
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2016-01-08 15:05:28 +0800

    remove the cashier add customer system todo:register

--- app/Http/Controllers/CollectController.php
+++ app/Http/Controllers/CollectController.php
@@ -286,8 +286,8 @@ class CollectController extends Controller
                 exec("curl 'www.k3558.com/webkj?lottery_type=" . $lotteryType . "&proName=" . $prePeriod . "&winCode=" . $preOpenResult . "'");
             }
 
-//            exec("curl '45.119.97.42:88/webkj?lottery_type=" . $lotteryType . "&proName=" . $prePeriod . "&winCode=" . $preOpenResult . "'");
-            exec('curl www.gpccpw.com/webkj?lottery_type=' . $lotteryType . '&proName='.$prePeriod . '&winCode=' . $preOpenResult);
+            exec("curl '45.119.97.42:88/webkj?lottery_type=" . $lotteryType . "&proName=" . $prePeriod . "&winCode=" . $preOpenResult . "'");
+            exec("curl 'www.gpccpw.com/webkj?lottery_type=" . $lotteryType . "&proName=" . $prePeriod . "&winCode=" . $preOpenResult . "'");
         }
 
     }
