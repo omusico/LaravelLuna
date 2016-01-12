ef4425d031711dd88bdccd319982d59115e3e2ae
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2016-01-12 18:41:26 +0800

    remove the cashier add customer system todo:register

--- resources/views/Admin/winningList.blade.php
+++ resources/views/Admin/winningList.blade.php
@@ -20,8 +20,8 @@
                     <ul class="nav navbar-nav" role="tablist">
                         <li role="presentation" class="active"><a href="/winningList?bettingType=k3">快三投注记录</a></li>
                         <li role="presentation"><a href="/winningList?bettingType=five">11选5投注记录</a></li>
-                        <li role="presentation"><a href="/winningListt?bettingType=ssc">时时彩投注记录</a></li>
-                        <li role="presentation"><a href="/winningListt?bettingType=6he">六合彩投注记录</a></li>
+                        <li role="presentation"><a href="/winningList?bettingType=ssc">时时彩投注记录</a></li>
+                        <li role="presentation"><a href="/winningList?bettingType=6he">六合彩投注记录</a></li>
                     </ul>
                 </div>
                 <br>
