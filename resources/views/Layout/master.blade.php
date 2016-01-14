e8061e43589d6396efc4fde9f1374074cad69f6a
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2016-01-14 14:21:38 +0800

    remove the cashier add customer system todo:register

--- resources/views/Layout/master.blade.php
+++ resources/views/Layout/master.blade.php
@@ -8,7 +8,7 @@
     <meta name="Description" content="快三娱乐平台。">
     <title> @yield('title') </title>
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/all.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/online3.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/online2.css') }}">
     @yield('css')
 </head>
 <body>
@@ -273,13 +273,13 @@
         var flag = 0;
         $('#rightArrow').on("click", function () {
             if (flag == 1) {
-                $("#floatDivBoxs").animate({right: '0'}, 300);
-                $(this).animate({right: '170px'}, 300);
+                $("#floatDivBoxs").animate({right: '-175px'}, 300);
+                $(this).animate({right: '-5px'}, 300);
 //                $(this).css('background-position', '-50px 0');
                 flag = 0;
             } else {
-                $("#floatDivBoxs").animate({right: '-175px'}, 300);
-                $(this).animate({right: '-5px'}, 300);
+                $("#floatDivBoxs").animate({right: '0'}, 300);
+                $(this).animate({right: '170px'}, 300);
 //                $(this).css('background-position', '0px 0');
                 flag = 1;
             }
