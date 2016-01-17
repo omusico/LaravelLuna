72af829eae250767ac1208de2491524dd984f8c8
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2016-01-17 10:08:26 +0800

    remove the cashier add customer system todo:register

--- resources/views/Lottery/k3gamerule.blade.php
+++ resources/views/Lottery/k3gamerule.blade.php
@@ -4,24 +4,13 @@
 @stop
 @section('css')
     <style type="text/css">
-        .cm_wrapper {
-            /*min-width: 100%;*/
-            position: relative;
-            /*border-bottom: 4px solid #530000;*/
-        }
-
-        .clear {
-            clear: both;
-            overflow: hidden;
-            zoom: 1;
-        }
     </style>
 @stop
 @section('content')
     <div class="container" style="text-align: center;">
         <div class="row">
             <div class="col-md-10 col-md-offset-1" style="background-color: white;padding:0px">
-                <img src="/css/k3rule2.jpg" alt="优惠活动" width="100%" height="100%"></div>
+                <img src="/css/k3rule2.jpg" alt="游戏规则" width="100%" height="100%"></div>
         </div>
     </div>
 @stop
