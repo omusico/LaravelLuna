e85f5ecdd3bf66ed5fbd3e114f44f1e410bcae63
zhouxinqi@tescar.cn <zhouxinqi@tescar.cn>
2016-01-23 10:57:27 +0800

    remove the cashier add customer system todo:register

--- app/Http/Controllers/LotteryFiveController.php
+++ app/Http/Controllers/LotteryFiveController.php
@@ -38,10 +38,9 @@ class LotteryFiveController extends Controller
         if (strtolower($request->lottery_type) == 'cqfive') {
             return view('errors.maintance');
         }
-//        return view('errors.maintance');
-//        if (strtolower($request->lottery_type) == 'fjk3') {
-//            return view('errors.maintance');
-//        }
+        if (strtolower($request->lottery_type) == 'zjfive') {
+            return view('errors.maintance');
+        }
         return view('Lottery.fivelotteryindex', compact('czName', 'config', 'chipins', 'fiveOdds', 'lotterystatus', 'lotterytypes'));
     }
 
